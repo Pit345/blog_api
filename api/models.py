80fd4ef45e8bb8eaa7d423d206edde32a5837bff
Pit345 <p1312402@gmail.com>
2024-02-27 17:06:30 +0200

    post serializers

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    body = models.TextField(blank=True, default='')
+    user = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from api.models import Post
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username']
+
+class PostSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.usernmame')
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'body', 'user']
