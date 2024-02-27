6cb0c5d8ba1a3f5f94e3c53dca4595a1a1deb115
Pit345 <p1312402@gmail.com>
2024-02-27 17:41:16 +0200

    add category model

--- api/models.py
+++ api/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from .models import Post
 
 # Create your models here.
 
@@ -14,3 +13,8 @@ class Comment(models.Model):
     body = models.TextField(blank=False)
     user = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
     comments = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, blank=False, default='')
+    owner = models.ForeignKey('auth.User', related_name='categories', on_delete=models.CASCADE)
+    posts = models.ManyToManyField('Post', related_name='categories', blank=True)
--- api/serializers.py
+++ api/serializers.py
@@ -2,13 +2,14 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from api.models import Post
 from api.models import Comment
+from api.models import Category
 
 class UserSerializer(serializers.ModelSerializer):
     posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
         model = User
-        fields = ['id', 'username', 'posts', 'comments']
+        fields = ['id', 'username', 'posts', 'comments', 'categories']
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -16,10 +17,18 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'body', 'user', 'comments']
+        fields = ['id', 'title', 'body', 'user', 'comments', 'categories']
 
 class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='username.username')
     class Meta:
         model = Comment
         fields = ['id', 'body', 'username', 'post']
+
+class CategorySerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'user', 'posts']
