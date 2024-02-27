c733b77bf81995b8ba92759d399cb1da697bc7a3
Pit345 <p1312402@gmail.com>
2024-02-27 17:56:04 +0200

    add postgresql

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'blog_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '10',
+        'HOST': 'localhost'
     }
 }
 
